92e141c1f01ffc75befcaab81f0aa92e0a915e7d
Christopher Young <christopheryoung@MacBook123.local>
2024-04-15 11:10:57 -0700

    added movement layout

--- src/components/controls/Movement.js
+++ src/components/controls/Movement.js
@@ -1,9 +1,26 @@
 import "./Movement.scss";
+// Icons
+import { FaLongArrowAltUp } from "react-icons/fa";
+import { FaLongArrowAltLeft } from "react-icons/fa";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { FaLongArrowAltDown } from "react-icons/fa";
+
 
 const Movement = () => {
   return (
     <div id="movement">
+      <div className="movement-top">
+        <button><FaLongArrowAltUp/></button>
+      </div>
 
+      <div className="movement-mid">
+        <button><FaLongArrowAltLeft/></button>
+        <button><FaLongArrowAltRight/></button>
+      </div>
+
+      <div className="movement-bot">
+        <button><FaLongArrowAltDown/></button>
+      </div>
     </div>
   );
 };

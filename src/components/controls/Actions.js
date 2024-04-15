54d11854b9aba2d3bbbce3c02c5155bd8f0df655
Christopher Young <christopheryoung@MacBook123.local>
2024-04-15 11:41:43 -0700

    added actions layout

--- src/components/controls/Actions.js
+++ src/components/controls/Actions.js
@@ -1,9 +1,24 @@
 import "./Actions.scss";
+// Icons
+import { GiBroadsword } from "react-icons/gi";
+import { GiHealthPotion } from "react-icons/gi";
 
 const Actions = () => {
   return (
     <div id="actions">
+      <div className="actions-ingame">
+        <div className="actions-ingame-top">
+          <button><GiBroadsword/></button>
+        </div>
 
+        <div className="actions-ingame-bot">
+          <button><GiHealthPotion/></button>
+        </div>
+      </div>
+
+      <div className="actions-newgame">
+        <button>New Game</button>
+      </div>
     </div>
   );
 };
